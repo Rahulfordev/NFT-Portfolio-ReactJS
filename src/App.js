4c60de87a14ccd687c1580a268ce2d10c8399877
rahul ali <82302787+Rahulfordev@users.noreply.github.com>
2023-01-29 20:36:37 +0600

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import {React} from 'react';
+import '../src/assets/icons/font-awesome.css';
+import './App.css';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+
+import Home from './pages/Home';
+
+function App() { 
+    return (
+       <Home />
+    );
+}
+
+export default App;
